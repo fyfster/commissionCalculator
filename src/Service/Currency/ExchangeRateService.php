d2ae65cff79d300b653c39a6211a5dbb2a38166e
Razvan Visean <visean.razvan@yahoo.com>
2025-05-15 22:37:58 +0300

    updated exchange source url

--- src/Service/Currency/ExchangeRateService.php
+++ src/Service/Currency/ExchangeRateService.php
@@ -8,16 +8,23 @@ class ExchangeRateService
 {
     private array $rates;
 
-    public function __construct(private HttpClientInterface $client)
-    {
+    public function __construct(
+        private HttpClientInterface $client,
+        private string $apiKey
+    ) {
         $this->fetchRates();
     }
 
     private function fetchRates(): void
     {
         // I saw in the Git project comments that the initial link https://api.exchangeratesapi.io/latest
-        // was not working so used another one
-        $response = $this->client->request('GET', 'https://developers.paysera.com/tasks/api/currency-exchange-rates');
+        // was not working so used another one from the comments in the alternitive issue
+        $response = $this->client->request('GET', 'https://api.apilayer.com/exchangerates_data/latest?base=EUR', [
+            'headers' => [
+                'Content-Type' => 'text/plain',
+                'apikey' => $this->apiKey,
+            ],
+        ]);
         $data = $response->toArray();
 
         $this->rates = $data['rates'] ?? [];
